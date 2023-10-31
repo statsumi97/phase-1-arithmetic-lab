0ccfdceec0b88f98043b193096ee54eaa5d367e7
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-10-31 15:45:10 -0400

    final product

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+//
+const random = Math.floor(Math.random()) + 1
+//
+let num3 = 8;
+let num4 = 2;
+const mod = num3 / num4;
+//
+const max = Math.max(5, 10, 15, 20);
